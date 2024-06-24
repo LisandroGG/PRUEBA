b50c859ac898dd938c435704fe9065a438976631
Lisandro Pereyra <lisandro.pereyra123456@gmail.com>
2024-06-24 20:30:31 -0300

    fix

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 import express from 'express'
 import trabajosRoutes from './src/routes/routes.js';
 import cors from 'cors';
-import fs from 'fs';
-import path from 'path';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const __dirname = process.cwd();
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -19,16 +16,6 @@ app.use(cors({
 // Middleware para manejar datos JSON
 app.use(express.json());
 
-// Directorio de uploads
-const uploadDir = path.join(__dirname, 'uploads');
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
-
-// Middleware para servir archivos estáticos desde /uploads
-app.use('/uploads', express.static(uploadDir));
-
-// Rutas de la aplicación
 app.use(trabajosRoutes);
 
 // Función principal para iniciar la aplicación
