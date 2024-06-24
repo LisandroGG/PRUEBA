1555c2a308119eb17263e2a10fa54467cb4fcb51
Lisandro Pereyra <lisandro.pereyra123456@gmail.com>
2024-06-24 19:54:26 -0300

    dale

--- src/index.js
+++ src/index.js
@@ -17,7 +17,18 @@ app.get("/", (req, res) => {
     res.send(htmlResponse)
 })
 
-app.get("/trabajos")
+app.get("/trabajos", (req, res) => {
+    const htmlHola = `
+    <html>
+    <head>
+    <title>Trabajos</title>
+    </head>
+    <body>
+    <h1>Entraste a trabajos</h1>
+    </body>
+    </html>`
+    res.send(htmlHola)
+})
 
 app.listen(port, () => {
     console.log(`port runing in ${port}`)
